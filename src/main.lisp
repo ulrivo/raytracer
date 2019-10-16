1956000cd4be30cd14008bbcde2e9e43b2706030
Ulrich Vollert <ulrivo@Ulrichs-MBP.fritz.box>
2019-10-16 22:11:00 +0200

    finished nearly chapter 2, drawing on canvas

--- src/main.lisp
+++ src/main.lisp
@@ -1,3 +1,49 @@
 (defpackage raytracer
   (:use :cl))
 (in-package :raytracer)
+
+(defstruct projectile
+  position velocity)
+
+(defstruct environment
+  gravity wind)
+
+(defparameter *pro*
+  (make-projectile :position (point 0 1 0)
+                   :velocity (normalize (vectorr 1 1 0))))
+
+(defparameter *env*
+  (make-environment :gravity (vectorr 0 -0.1 0)
+                    :wind (vectorr -0.01 0 0)))
+
+(defun tick (en pro)
+  (make-projectile
+   :position (tadd (projectile-position pro) (projectile-velocity pro))
+   :velocity (tadd (projectile-velocity pro)
+                   (tadd (environment-wind en) (environment-gravity en)))))
+
+(defun shot (w h)
+  (let* ((c (canvas w h))
+         (start (point 0 1 0))
+         (velocity (mults (normalize (vectorr 1 1.8 0)) 11.25))
+         (p (make-projectile :position start :velocity velocity))
+         (gravity (vectorr 0 -0.1 0))
+         (wind (vectorr -0.01 0 0))
+         (e (make-environment :gravity gravity :wind wind)))
+    (loop while (< 0 (elt (projectile-position p) 1))
+          do
+             (setf p (tick e p))
+             (write-pixel c
+                          (elt (projectile-position p) 0)
+                          (elt (projectile-position p) 1)
+                          (color 1 0 0)))
+             ;; (format t "x ~A, y ~A~%"
+             ;;         (elt (projectile-position p) 0)
+             ;;         (elt (projectile-position p) 1)))
+    (save-canvas c "test.ppm")))
+
+(defun test ()
+  (let ((c (canvas 4 4)))
+    (write-pixel c 0 1.9 (color 1 0 0))
+    (write-pixel c 0 2.6 (color 1 0 0))
+    c))
--- src/tuples.lisp
+++ src/tuples.lisp
@@ -53,3 +53,66 @@
    (- (* (elt a 1) (elt b 2)) (* (elt a 2) (elt b 1)))
    (- (* (elt a 2) (elt b 0)) (* (elt a 0) (elt b 2)))
    (- (* (elt a 0) (elt b 1)) (* (elt a 1) (elt b 0)))))
+
+;; color
+
+(defun color (x y z)
+  (vector x y z))
+
+(defun hadamard-product (c d) (map 'vector #'* c d))
+
+;; canvas
+
+(defun canvas (w h)
+  (make-array (list w h) :initial-element (vector 0 0 0)))
+
+(defun write-pixel (canvas x y color)
+  (let* ((dimension (array-dimensions canvas))
+         (w (first dimension))
+         (h (second dimension))
+         (xi (truncate x))
+         (yi (truncate y)))
+    (and (<= 0 xi) (< xi w) (<= 0 yi) (< yi h)
+         (setf (aref canvas xi yi) color))))
+
+;; given a string, if it is longer than 70 characters,
+;; find the last space before 70 character and replace it with a newline,
+;; repeat this on rest
+;; answer a string
+
+(defun last-space (string)
+  (position #\ (subseq string 0 70) :from-end t))
+
+(defun no-longer (string)
+  (if (< 70 (length string))
+      (let ((pos (last-space string)))
+        (format nil "~A~%~A"
+                (subseq string 0 pos)
+                (no-longer (subseq string (1+ pos)))))
+      string))
+
+(defun canvas-to-ppm (canvas)
+  (let* ((dimension (array-dimensions canvas))
+         (w (first dimension))
+         (h (second dimension))
+         (data (format nil "P3~%~A ~A~%255~%" w h)))
+    (flet ((concat (s)
+             (setf data
+                   (concatenate
+                    'string
+                    data s))))
+      (dotimes (i w)
+        (let ((line nil))
+          (dotimes (j h)
+            (let ((col (aref canvas i j)))
+              (dotimes (k 3)
+                (push (truncate (* 255 (elt col k))) line))))
+          (concat (no-longer (format nil "~{~A ~}~%" (reverse line)))))))
+    data))
+
+(defun save-canvas (canvas filename)
+  (with-open-file (str filename
+                       :direction :output
+                       :if-exists :supersede
+                       :if-does-not-exist :create)
+    (format str "~A" (canvas-to-ppm canvas))))
